816e23090723a492f10ee5a832bf9b265f059d87
Md. Samiur Rahman Mukul <97078249+Samiur-SiliconOrchard@users.noreply.github.com>
2022-06-28 20:38:05 +0600

    mym-mart admin panel inital commit

---
+++ MYM-Mart-AdminPanel/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1
+        style={{
+          textAlign: "center",
+        }}>
+        MYM-Mart AdminPanel
+      </h1>
+    </div>
+  );
+}
+
+export default App;
