932c116b9e688f157b3dd011eaafde5896a9cd8a
Md. Samiur Rahman Mukul <97078249+Samiur-SiliconOrchard@users.noreply.github.com>
2023-01-23 16:54:11 +0600

    respositry previous code remoed for new start fresh

--- MYM-Mart-AdminPanel/src/components/category/AddCategory.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function AddCategory() {
-  return (
-    <div className='text-2xl text-center pt-2'>Add Category</div>
-  );
-}
-
-export default AddCategory;
--- MYM-Mart-AdminPanel/src/components/category/CategoriesProducts.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function CategoriesProducts() {
-  return (
-    <div className='text-2xl text-center pt-2'>Categories Products</div>
-  );
-}
-
-export default CategoriesProducts;
