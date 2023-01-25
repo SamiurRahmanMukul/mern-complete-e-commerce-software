5ed85f63e0c7cc3e8b21bc337f98fe2ad4935f33
Md. Samiur Rahman Mukul <97078249+Samiur-SiliconOrchard@users.noreply.github.com>
2023-01-25 18:11:41 +0600

    route protected middlware make & protected route

--- mym-mart-admin-panel/src/App.js
+++ mym-mart-admin-panel/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PrivateRoute from './components/middleware/PrivateRoute';
+import PublicRoute from './components/middleware/PublicRoute';
 import Dashboard from './pages/Dashboard';
 import Error from './pages/Error';
 import Home from './pages/Home';
@@ -12,10 +14,10 @@ function App() {
         <Route path='/' element={<Home />} />
 
         {/* DASHBOARD ROUTE */}
-        <Route path='/dashboard/:tab' element={(<Dashboard />)} />
+        <Route path='/dashboard/:tab' element={(<PrivateRoute><Dashboard /></PrivateRoute>)} />
 
         {/* LOGIN ROUTE */}
-        <Route path='/auth/login' element={(<Login />)} />
+        <Route path='/auth/login' element={(<PublicRoute><Login /></PublicRoute>)} />
 
         {/* ERROR ROUTE */}
         <Route path='*' element={<Error />} />
