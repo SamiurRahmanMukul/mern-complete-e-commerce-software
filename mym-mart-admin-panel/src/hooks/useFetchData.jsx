95cc05a5c70c5a15b4edd50df663594b27cf7107
Md. Samiur Rahman Mukul <97078249+Samiur-SiliconOrchard@users.noreply.github.com>
2023-02-14 07:29:59 +0600

    useFetchDate hooks make for API data fetch

---
+++ mym-mart-admin-panel/src/hooks/useFetchData.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import ApiService from '../utils/apiService';
+
+const useFetchData = (url, reFetch) => {
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    ApiService.get(url)
+      .then((res) => {
+        setLoading(false);
+        if (res?.result_code === 0) {
+          setResponse(res?.result);
+        } else {
+          setError('Sorry! Something went wrong. App server error');
+        }
+      })
+      .catch((err) => {
+        setError(err?.response?.data?.result?.error || 'Sorry! Something went wrong. App server error');
+        setLoading(false);
+      });
+  }, [url, reFetch]);
+
+  return [loading, error, response];
+};
+
+export default useFetchData;
